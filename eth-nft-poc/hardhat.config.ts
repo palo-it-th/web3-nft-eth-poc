cd2a021a4a0d4c9bc06d22f8ccc567c09d11e45b
Peter Weismann <peter.weismann@aia.com>
2022-09-27 18:04:05 +0700

    Add exercise code

--- eth-nft-poc/hardhat.config.ts
+++ eth-nft-poc/hardhat.config.ts
@@ -1,5 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "solidity-coverage"
+
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
