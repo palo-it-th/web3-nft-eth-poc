4379b1aebf5fbdd2526116ba4b3a72067d3591ce
Peter Weismann <peter.weismann@aia.com>
2022-09-27 18:37:12 +0700

    Adjust solhint

--- eth-nft-poc/contracts/Lock.sol
+++ eth-nft-poc/contracts/Lock.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.8.9;
 
 // Uncomment this line to use console.log
-import "hardhat/console.sol";
+import 'hardhat/console.sol';
 
 contract Lock {
     uint256 public unlockTime;
@@ -10,10 +10,7 @@ contract Lock {
     event Withdrawal(uint256 amount, uint256 when);
 
     constructor(uint256 _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
+        require(block.timestamp < _unlockTime, 'Unlock time should be in the future');
 
         unlockTime = _unlockTime;
         owner = payable(msg.sender);
@@ -21,11 +18,7 @@ contract Lock {
 
     function withdraw() public {
         // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        console.log(
-            "Unlock time is %o and block timestamp is %o",
-            unlockTime,
-            block.timestamp
-        );
+        console.log('Unlock time is %o and block timestamp is %o', unlockTime, block.timestamp);
 
         require(block.timestamp >= unlockTime, "You can't withdraw yet");
         require(msg.sender == owner, "You aren't the owner");
