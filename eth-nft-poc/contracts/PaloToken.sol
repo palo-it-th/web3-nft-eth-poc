88fafc48d11878c60e1f24d1018d28cca4c96194
Peter Weismann <peter.weismann@aia.com>
2022-09-27 18:28:51 +0700

    Add solhint and prettier

--- eth-nft-poc/contracts/PaloToken.sol
+++ eth-nft-poc/contracts/PaloToken.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.9;
 
-import "hardhat/console.sol";
+import 'hardhat/console.sol';
 
 contract PaloToken {
     address public owner;
@@ -9,24 +9,17 @@ contract PaloToken {
     mapping(address => uint256) userBalances;
 
     event Mint(address receiver, uint256 amount, uint256 when);
-    event Transfer(
-        address sender,
-        address receiver,
-        uint256 amount,
-        uint256 when
-    );
 
-    constructor(address _owner, uint _maximumSupply) {
+    event Transfer(address sender, address receiver, uint256 amount, uint256 when);
+
+    constructor(address _owner, uint256 _maximumSupply) {
         maximumSupply = _maximumSupply;
         owner = _owner;
     }
 
     function mint(address _receiver, uint256 _amount) external {
-        require(owner == msg.sender, "Only the owner can mint tokens!");
-        require(
-            _amount + currentSupply <= maximumSupply,
-            "Maximum supply cap has been reached!"
-        );
+        require(owner == msg.sender, 'Only the owner can mint tokens!');
+        require(_amount + currentSupply <= maximumSupply, 'Maximum supply cap has been reached!');
         userBalances[_receiver] += _amount;
         currentSupply += _amount;
         emit Mint(_receiver, _amount, block.timestamp);
@@ -37,10 +30,7 @@ contract PaloToken {
         address _receiver,
         uint256 _amount
     ) external {
-        require(
-            userBalances[_sender] >= _amount,
-            "You don't have sufficient funds!"
-        );
+        require(userBalances[_sender] >= _amount, "You don't have sufficient funds!");
 
         userBalances[_sender] -= _amount;
         userBalances[_receiver] += _amount;
--- eth-nft-poc/hardhat.config.ts
+++ eth-nft-poc/hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "solidity-coverage"
+import "@nomiclabs/hardhat-solhint";
 
 
 const config: HardhatUserConfig = {
