09950893135582d7e523f3dba17d7406748eaca5
Peter Weismann <pweismann@palo-it.com>
2022-11-29 20:05:27 +0700

    Add script to run e2e tests

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,8 @@ FROM synthetixio/docker-e2e:16.17-ubuntu as base
 RUN mkdir /app
 WORKDIR /app
 
-COPY node_modules ./node_modules
+# Don't copy as everything is already copied in the last step
+# COPY node_modules ./node_modules
 
 FROM base as test
 COPY . .
---
+++ scripts/run_e2e_tests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+nohup npx hardhat node & > hardhat_node.log &
+npx jsipfs init
+npx jsipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin "[\"*\"]"
+npx jsipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "POST", "GET"]'
+nohup npx jsipfs daemon & > jsipfs_daemon.log &
+npx hardhat --network localhost run ../scripts/deploy_palonft.js
+cd ./frontend
+npx synpress open -cf cypress.config.ts
