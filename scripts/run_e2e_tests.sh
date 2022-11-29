1251824dd2d2a779d1d1f8f5c9bba2379023cd69
Peter Weismann <pweismann@palo-it.com>
2022-11-30 00:30:01 +0700

    Update pipeline

--- scripts/run_e2e_tests.sh
+++ scripts/run_e2e_tests.sh
@@ -5,6 +5,6 @@ npx jsipfs init
 npx jsipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin "[\"*\"]"
 npx jsipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "POST", "GET"]'
 nohup npx jsipfs daemon & > jsipfs_daemon.log &
-npx hardhat --network localhost run ../scripts/deploy_palonft.js
+npx hardhat --network localhost run deploy_palonft.js
 cd ../frontend
-npx synpress run -cf cypress.config.ts
+npm run start-and-test
